9591048a7c590f4207089459e80781266c220557
Hsin-Yun Lai <lai1029700@gmail.com>
2024-12-22 09:19:35 +0800

    feat: update site configuration and adjust asset paths for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://lai10297000.github.io",
+  base: "/frontend-mentor-practice",
   experimental: {
     svg: true,
   },
