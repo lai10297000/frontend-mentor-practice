ffd68843b30b6cd9a58c18c4ed9c9631584b0961
Hsin-Yun Lai <lai1029700@gmail.com>
2025-01-11 17:53:38 +0800

    chore: add React support and update dependencies in project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lai10297000.github.io",
   base: "/frontend-mentor-practice",
+
   experimental: {
     svg: true,
   },
+
+  integrations: [react()],
 });
